ab5ea8f6e2e6457355825ed513e20a277a7597b6
slmwld <slmwld>
2019-01-30 21:06:14 -0600

    segundo commint

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -6,8 +6,18 @@ import { NavController, NavParams } from 'ionic-angular';
   templateUrl: 'home.html',
 })
 export class HomePage {
+  private _datacards: any;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  
+  constructor(public navCtrl: NavController, public navParams: NavParams){
+
+    this._datacards = [
+      {title: "Imagen 1", image: "adult-attractive-background-544117.png", descripcion: "Lorem."},
+      {title: "Imagen 2", image: "aroma-art-beverage-1251175.png",         descripcion: "Lorem."},
+      {title: "Imagen 3", image: "art-blur-cappuccino-302899.png",         descripcion: "Lorem."},
+      {title: "Imagen 4", image: "attractive-bar-barista-296888.png",      descripcion: "Lorem."},
+      {title: "Imagen 5", image: "beverage-coffee-computer-877695.png",    descripcion: "Lorem."},
+    ];
   }
 
   ionViewDidLoad() {
